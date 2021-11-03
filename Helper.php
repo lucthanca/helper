3da9b48edac7c1f391858db2da9a577b05d540e9
vadu <dunghv@bsscommerce.com>
2021-11-03 15:07:58 +0700

    improve log function, allow dumphtml logger

--- Helper.php
+++ Helper.php
@@ -1,45 +1,113 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
-use Zend\Log\Logger;
-use Zend\Log\Writer\Stream;
+
+use \Logger as LoggerPhp;
+
+use Symfony\Component\VarDumper\Cloner\VarCloner;
+use Symfony\Component\VarDumper\Dumper\HtmlDumper;
 
 if (!function_exists('vadu_log')) {
-    /**
-     * Log to vadu_log.log file
-     *
-     * @param mixed $var
-     * @author Vadu
-     */
-    function vadu_log($var, ...$moreVars)
-    {
-        $writer = new Stream(BP . '/var/log/vadu_log.log');
-        $logger = new Logger();
-
-        v_log($var, $writer, $logger);
-
+    function vadu_log($var, ...$moreVars) {
+        LoggerPhp::configure(
+            [
+                'rootLogger' => ['appenders' => ['vaduLog']],
+                'appenders' => [
+                    'vaduLog' => [
+                        'class' => 'LoggerAppenderFile',
+                        'layout' => [
+                            'class' => 'LoggerLayoutPattern'
+                        ],
+                        'params' => [
+                            'file' => __DIR__ . '/var/log/vadu.log',
+                            'append' => true
+                        ]
+                    ]
+                ]
+            ]
+        );
+        $logger = LoggerPhp::getLogger('vaduLog');
+        $logger->info($var);
         foreach ($moreVars as $var) {
-            v_log($var, $writer, $logger);
-        }
-
-        if (1 < func_num_args()) {
-            return func_get_args();
+            $logger->info($var);
         }
     }
 }
 
-if (!function_exists('v_log')) {
+//if (!function_exists('vadu_log')) {
+//    /**
+//     * Log to vadu_log.log file
+//     *
+//     * @param mixed $var
+//     * @author Vadu
+//     */
+//    function vadu_log($var, ...$moreVars)
+//    {
+//        $writer = new \Laminas\Log\Writer\Stream(__DIR__ . '/var/log/vadu_log.log');
+//        $logger = new \Laminas\Log\Logger();
+//
+//        v_log($var, $writer, $logger);
+//
+//        foreach ($moreVars as $var) {
+//            v_log($var, $writer, $logger);
+//        }
+//
+//        if (1 < func_num_args()) {
+//            return func_get_args();
+//        }
+//    }
+//}
+//
+//if (!function_exists('v_log')) {
+//    /**
+//     * @param mixed $var
+//     * @param object $writer
+//     * @param object $logger
+//     */
+//    function v_log($var, $writer, $logger)
+//    {
+//        $logger->addWriter($writer);
+//        if (is_string($var)) {
+//            $logger->info($var);
+//        } else {
+//            $logger->info(print_r($var, true));
+//        }
+//    }
+//}
+
+if (!function_exists('vadu_html')) {
     /**
+     * Log to var/log/vadu.log file
+     *
+     * // Chú ý cần custom trong file LoggerLayoutHtml:157
+     *
      * @param mixed $var
-     * @param object $writer
-     * @param object $logger
+     * @throws Exception
+     * @author Vadu
      */
-    function v_log($var, $writer, $logger)
+    function vadu_html($var, ...$moreVars): void
     {
-        $logger->addWriter($writer);
-        if (is_string($var)) {
-            $logger->info($var);
-        } else {
-            $logger->info(print_r($var, true));
+        LoggerPhp::configure(
+            [
+                'rootLogger' => ['appenders' => ['vaduLog']],
+                'appenders' => [
+                    'vaduLog' => [
+                        'class' => 'LoggerAppenderFile',
+                        'layout' => [
+                            'class' => 'LoggerLayoutHtml'
+                        ],
+                        'params' => [
+                            'file' => __DIR__ . '/var/log/vadu.html',
+                            'append' => true
+                        ]
+                    ]
+                ]
+            ]
+        );
+        $logger = LoggerPhp::getLogger('vaduLog');
+        $logger->info(VarDumperLogToHtml::getLogHtmlContent($var));
+
+        foreach ($moreVars as $var) {
+            $logger->info(VarDumperLogToHtml::getLogHtmlContent($var));
         }
     }
 }
@@ -86,5 +154,53 @@ if (!function_exists('vadu_execution_time')) {
         $callback();
         $endTime = microtime(true);
         return ($endTime - $startTime);
+    }
+}
+
+class VarDumperLogToHtml {
+    /**
+     * @throws ErrorException
+     */
+    public static function dump($var, $file = null): void
+    {
+        static $output = null;
+        $cloner        = new VarCloner();
+        $dumper        = new HtmlDumper();
+
+        if (null === $output) {
+            if (!$file) {
+                $now = new \DateTime();
+                /** @psalm-suppress InvalidOperand */
+                $file = __DIR__ . "/var/log/" . $now->format('Y-m-d\TH-i-s-u') . '_' . random_int(0, mt_getrandmax()) . '.html';
+            }
+
+            $output = fopen($file, 'a+b');
+        }
+
+        if (false === is_resource($output)) {
+            throw new \RuntimeException('Something went wrong creating the dump file.');
+        }
+
+        $dumper->dump($cloner->cloneVar($var), $output, []);
+    }
+
+    public static function getLogHtmlContent($var): ?string
+    {
+        $now = new \DateTime();
+        /** @psalm-suppress InvalidOperand */
+        $file = __DIR__ . "/var/log/" . $now->format('Y-m-d\TH-i-s-u') . '_' . random_int(0, mt_getrandmax()) . '.html';
+        try {
+            VarDumperLogToHtml::dump($var, $file);
+        } catch (\Exception $e) {
+            return null;
+        }
+
+        if (file_exists($file)) {
+            $content = file_get_contents($file);
+            unlink($file);
+            return $content;
+        }
+
+        return null;
     }
 }
